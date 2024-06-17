bc9741032ce0f6910df8e5533795c4b7554b0964
Shakthi B <babuk1769@gmail.com>
2024-06-17 14:41:17 +0530

    data ingestion completed

---
+++ src/cnn_Classifier/constants/__init__.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+CONFIG_FILE_PATH= Path(r"C:\Users\91638\OneDrive\Documents\Chicken-Disease-Classification-Project\Chicken-Disease-Classification-Project\config\config.yaml")
+PARAMS_FILE_PATH= Path(r"C:\Users\91638\OneDrive\Documents\Chicken-Disease-Classification-Project\Chicken-Disease-Classification-Project\params.yaml")
